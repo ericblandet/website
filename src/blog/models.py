9a93483531fd5278540459e94778b3d880770b6d
eric <eric.blandet@gmail.com>
2023-02-07 15:57:27 +0100

    first custom migration

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    published = models.BooleanField(default=False)
+    date = models.DateField(blank=True)
+    content = models.TextField()
+    descripion = models.TextField(default="")
